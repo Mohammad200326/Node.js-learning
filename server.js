37200214ae0a65df2839a726cb323719d511c6ae
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2025-07-02 16:08:52 +0300

    simple app

--- server.js
+++ server.js
@@ -3,7 +3,44 @@ const http = require("http");
 const port = 5050;
 
 const server = http.createServer((req, res) => {
-  res.end("Welcome to page!");
+  switch (req.url) {
+    case "/":
+      res.end(`
+        <html>
+            <head>
+                <title>My First App</title>
+            </head>
+            <body>
+                <h1>Hello!!</h1>
+            </body>
+        </html>
+    `);
+      break;
+    case "/user":
+      res.end(`
+        <html>
+            <head>
+                <title>My First App</title>
+            </head>
+            <body>
+                <h1>Hello User!!</h1>
+            </body>
+        </html>
+    `);
+      break;
+    default:
+      res.end(`
+        <html>
+            <head>
+                <title>My First App</title>
+            </head>
+            <body>
+                <h1>Undefined page!</h1>
+            </body>
+        </html>
+    `);
+      break;
+  }
 });
 
 server.listen(port, () => {
